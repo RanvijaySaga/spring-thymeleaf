a4eb4c462110a8ec8e52395564f3ed53bb5b2403
RanvijaySaga <ranvijay.saga@gmail.com>
2025-02-04 23:49:51 +0100

    adding basic structure for of student management to show working of thyme leaf with spring boot.

---
+++ springboot-thymeleaf/src/main/java/com/spring/play/springboot_thymeleaf/exception/StudentManagementException.java
@@ -0,0 +1,8 @@
+package com.spring.play.springboot_thymeleaf.exception;
+
+public class StudentManagementException extends RuntimeException {
+
+    public StudentManagementException(String message) {
+        super(message);
+    }
+}
